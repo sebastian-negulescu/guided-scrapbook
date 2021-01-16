7ba4c1c6cf2477a85ca56e578ac4761d0536d569
Sebastian Negulescu <sebastian.negulescu@accedo.tv>
2021-01-16 10:26:25 -0500

    Initial Commit

---
+++ christina/christinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  christinaApp.swift
+//  christina
+//
+//  Created by Sebastian Negulescu on 2021-01-16.
+//
+
+import SwiftUI
+
+@main
+struct christinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
